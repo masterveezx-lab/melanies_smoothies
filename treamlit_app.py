a6f65e4a6261501db311e820aa82dbd458b6a4e8
masterveezx-lab <masterveezx@gmail.com>
2025-11-30 18:15:12 +0700

    Update treamlit_app.py

--- treamlit_app.py
+++ treamlit_app.py
@@ -39,11 +39,12 @@ else:
     st.success('There are no pending orders right now', icon="👍")
 
 
+
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
